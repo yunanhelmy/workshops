e388259a51df551f4ebc02fa8a8cb4a0c2bde1ae
yunanhelmy <yunan@softwareseni.com>
2014-08-10 15:40:03 +0700

    fix model product, add validation for price and add method average rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,13 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates_presence_of :title, :description, :price
+
+  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0, less_than: 10}
+
+  def average_rating
+  	self.reviews.average(:rating)
+  end
+
 end
